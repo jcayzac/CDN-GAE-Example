fe267ae5add84a47a60e82e588365a4d9e5d2532
Julien Cayzac <julien.cayzac@gmail.com>
2011-10-02 17:59:51 +0900

    links auto-expand, IE8 and lots of sweets

--- src/crotte.js
+++ src/crotte.js
@@ -1,8 +1,10 @@
-/*jslint devel: true, browser: true, unparam: false, forin: true, plusplus: true, maxerr: 50, indent: 4 */
+/*jslint devel: true, browser: true, regexp: true, unparam: false, forin: true, plusplus: true, maxerr: 50, indent: 4 */
+
 (function () {
 	"use strict";
 	var F = false,
 		domIsReady = F,
+		can_drag = document.documentElement.draggable !== undefined,
 		base = '//cdn-jcayzac.appspot.com/files/',
 		store = function (k, v) {
 			try {
@@ -65,22 +67,158 @@
 		iframe = function (src, classname) {
 			var e = document.createElement("iframe");
 			e.src = src;
-			e.frameborder = 0;
+			e.frameBorder = 0;
 			e.allowfullscreen = 1;
+			e.scrolling = 'no';
+			e.seamless = 1;
 			e.className = classname;
 			return e;
 		},
-		wrap_16x9 = function (e, classname) {
-			var x = document.createElement('div'),
-				i = document.createElement('img');
-			x.className = 'keep-aspect-ratio ' + classname;
-			i.src = base + '16x9.png';
-			x.appendChild(i);
-			x.appendChild(e);
-			return x;
+		expand_iframe = function (src, classname) {
+			var outer = document.createElement('div'),
+				inner = document.createElement('div');
+			outer.className = 'expand';
+			inner.appendChild(iframe(src, classname));
+			outer.appendChild(inner);
+			return outer;
 		},
-		iframe_16x9 = function (s, c) {
-			return wrap_16x9(iframe(s, c), c);
+		no_expand_re = new RegExp(/\bnoexpand\b/),
+		expand_reference = function (pattern, cb) {
+			var e = document.querySelectorAll("article section a[href*='" + pattern + "']"),
+				j,
+				x,
+				next_sibling,
+				first_child,
+				parent_node,
+				ancestor,
+				uncle,
+				r,
+				valid_ancestors = {
+					body: true,
+					div: true,
+					section: true,
+					nav: true,
+					header: true,
+					footer: true,
+					aside: true,
+				};
+			for (j = 0; j < e.length; ++j) {
+				x = e[j];
+				if (!(x.className || '').match(no_expand_re)) {
+					r = cb(x.href);
+					if (r.element) {
+						r.element.className = [x.className || '', r.element.className || ''].join(' ');
+						uncle = next_sibling = x.nextSibling;
+						ancestor = parent_node = x.parentNode;
+						while (valid_ancestors[ancestor.tagName.toLowerCase()] === undefined) {
+							uncle = ancestor.nextSibling;
+							ancestor = ancestor.parentNode;
+						}
+						// detach element
+						x = parent_node.removeChild(x);
+						// replace it with its content
+						while ((first_child = x.firstChild) !== null) {
+							first_child = x.removeChild(first_child);
+							if (next_sibling) {
+								parent_node.insertBefore(first_child, next_sibling);
+							} else {
+								parent_node.appendChild(first_child);
+							}
+						}
+						// add referenced content
+						if (uncle) {
+							ancestor.insertBefore(r.element, uncle);
+						} else {
+							ancestor.appendChild(r.element);
+						}
+						if (r.ready) {
+							r.ready();
+						}
+					}
+				}
+			}
+		},
+		youtube_re = new RegExp(/watch\?v=([^&]+)/),
+		vimeo_re = new RegExp(/vimeo.com\/([0-9]+)/),
+		gist_re = new RegExp(/gist.github.com\/([0-9]+)/),
+		expand_references = function () {
+			expand_reference('youtube.com/watch', function (href) {
+				var x = {
+					element: expand_iframe(
+						[
+							"//www.youtube.com/embed/",
+							href.match(youtube_re)[1],
+							"?hd=1&autohide=1&fs=1&iv_load_policy=3&loop=1&rel=0&showsearch=0& showinfo=0&modestbranding=1"
+						].join(''),
+						'youtube'
+					)
+				};
+				x.element.role = 'application';
+				return x;
+			});
+			expand_reference('vimeo.com/', function (href) {
+				var x = {
+					element: expand_iframe(
+						[
+							'//player.vimeo.com/video/',
+							href.match(vimeo_re)[1],
+							'?title=0&byline=0&portrait=0&loop=1'
+						].join(''),
+						'vimeo'
+					)
+				};
+				x.element.role = 'application';
+				return x;
+			});
+			expand_reference('gist.github.com/', function (href) {
+				var code = href.match(gist_re)[1],
+					cb = 'on_gist_' + code + '_' + Math.floor(Math.random() * 99999999 + 1),
+					x = {
+						element: document.createElement('div'),
+						ready: function () {
+							load_script('https://gist.github.com/' + code + '.json?callback=' + cb);
+						}
+					};
+				x.element.id = 'gist-container-' + code;
+				window[cb] = function (x) {
+					var pre, div, i, filename, link, sendlink = function (e) {
+						e.dataTransfer.setData('DownloadURL', this.href);
+					};
+					window[cb] = null;
+
+					if (!x.div) {
+						return undefined;
+					}
+
+					div = document.querySelector('div#gist-container-' + code);
+					div.innerHTML = x.div;
+					pre = div.querySelectorAll('.gist-data');
+					for (i = 0; i < pre.length; i++) {
+						filename = x.files[i];
+						if (filename.substr(0, 8) !== 'gistfile') {
+							link = document.createElement('a');
+							link.href = 'https://raw.github.com/gist/' + code + '/' + filename;
+							link.innerHTML = filename;
+							link.target = '_blank';
+							link.setAttribute('data-repo', code);
+							link.setAttribute('data-filename', filename);
+							link.className = 'gist-raw-link font-sans';
+							if (can_drag) {
+								link.className += ' dragout';
+								link.setAttribute('draggable', 'true');
+								link.setAttribute('data-downloadurl', [
+									'text/plain',
+									filename,
+									link.href
+								].join(':'));
+								link.addEventListener('dragstart', sendlink, false);
+							}
+							pre[i].appendChild(link);
+						}
+					}
+				};
+				return x;
+			});
 		};
 
 	(function () {
@@ -141,77 +279,6 @@
 			return undefined;
 		}
 		domIsReady = true;
-		var crotty_divs = document.querySelectorAll('div[data-crotte]'),
-			i,
-			e,
-			r,
-			code,
-			cb,
-			cached,
-			create_gist_handler = function (e, code, cb, cached) {
-				return function (x) {
-					var pre,
-						div,
-						i,
-						filename,
-						link;
-					window[cb] = null;
-
-					if (!x.div) {
-						return undefined;
-					}
-					if (!cached) {
-						store('gist-' + code, x);
-					}
-
-					div = document.createElement('div');
-					div.innerHTML = x.div;
-					pre = div.querySelectorAll('.gist-data');
-					for (i = 0; i < pre.length; i++) {
-						filename = x.files[i];
-						if (filename.substr(0, 8) !== 'gistfile') {
-							link = document.createElement('a');
-							link.href = 'https://raw.github.com/gist/' + code + '/' + filename;
-							link.innerHTML = filename;
-							link.target = '_blank';
-							link.setAttribute('data-repo', code);
-							link.setAttribute('data-filename', filename);
-							link.className = 'gist-raw-link font-sans';
-							pre[i].appendChild(link);
-						}
-					}
-					e.parentNode.insertBefore(div.firstChild, e);
-					e.parentNode.removeChild(e);
-				};
-			};
-
-		for (i = 0; i < crotty_divs.length; i++) {
-			e = crotty_divs[i];
-			r = null;
-			switch (e.getAttribute('data-crotte')) {
-			case 'youtube':
-				r = iframe_16x9('//www.youtube.com/embed/' + e.getAttribute('data-ref') + '?hd=1&autohide=1&fs=1&iv_load_policy=3&loop=1&rel=0&showsearch=0& showinfo=0&modestbranding=1', 'youtube');
-				break;
-			case 'vimeo':
-				r = iframe_16x9('//player.vimeo.com/video/' + e.getAttribute('data-ref') + '?title=0&byline=0&portrait=0&loop=1', 'vimeo');
-				break;
-			case 'gist':
-				code = e.getAttribute('data-ref');
-				cb = 'on_gist_' + code + '_' + Math.floor(Math.random() * 99999999 + 1);
-				cached = retrieve('gist-' + code);
-				window[cb] = create_gist_handler(e, code, cb, cached);
-
-				if (cached) {
-					window[cb](cached);
-				} else {
-					load_script('https://gist.github.com/' + code + '.json?callback=' + cb);
-				}
-				break;
-			}
-			if (r) {
-				e.parentNode.insertBefore(r, e);
-				e.parentNode.removeChild(e);
-			}
-		}
+		expand_references();
 	}));
 }());
